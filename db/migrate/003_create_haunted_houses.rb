129fb6519f2c43140368b7f31dd35f5ea073c166
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-08 15:38:17 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,8 +6,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :location
       t.string :theme
       t.integer :price
-      t.boolean :friendly
-      t.datetime :opening_time
+      t.boolean :family_friendly
+      t.datetime :opening_date
       t.datetime :closing_date
       t.string :description
           end
